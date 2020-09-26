70578a74828718dae47bb72c97834a3909462231
Nyash-Mauro <mauricee423@gmail.com>
2020-09-26 17:20:28 +0300

    addee components to my app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { CountInputComponent } from './count-input/count-input.component';
 
 @NgModule({
-  declarations: [AppComponent, QuoteComponent],
+  declarations: [AppComponent, QuoteDetailComponent, QuoteFormComponent, CountInputComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
