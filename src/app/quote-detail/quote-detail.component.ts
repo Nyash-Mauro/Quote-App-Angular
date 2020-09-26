34df1371184a628ab030a5a841b12ddddb792843
Nyash-Mauro <mauricee423@gmail.com>
2020-09-26 19:50:11 +0300

    added styles to css

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
-  styleUrls: ['./quote-detail.component.css']
+  styleUrls: ['./quote-detail.component.css'],
 })
 export class QuoteDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+  quoteDelete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
+  @Output() voteIncrease = new EventEmitter<boolean>();
+  @Output() voteDecrease = new EventEmitter<boolean>();
+  ifIncrease(one: boolean) {
+    this.voteIncrease.emit(one);
+  }
+  ifDecrease(two: boolean) {
+    this.voteDecrease.emit(two);
+  }
+  quoteDan(purge: boolean) {
+    this.isComplete.emit(purge);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
