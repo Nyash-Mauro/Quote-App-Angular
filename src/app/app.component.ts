89c274f22da3c96f0d9e6030d243075b71ffaf8e
Nyash-Mauro <mauricee423@gmail.com>
2020-09-26 23:22:18 +0300

    added forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,19 @@ export class AppComponent {
       1,
       'Dont look at the clock, do what it does keep moving',
       'Biron Lovine',
-      new Date(2020, 4, 21)
+      new Date(2023, 4, 12)
     ),
     new Quote(
       2,
       'Difficulties in your life do not come to destroy you but to help you realize your potential and future',
       'Rihannah',
-      new Date(2019, 5, 16)
+      new Date(1991, 5, 16)
     ),
     new Quote(
       3,
       'Success is a battle located in the brain only the fittest can survive',
       'Lovine Biron',
-      new Date(2020, 1, 21)
+      new Date(1550, 1, 21)
     ),
   ];
 }
